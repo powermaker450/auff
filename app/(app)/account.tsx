ef0a10c0f1d3682f2ede3b5736afa9bd910703f2
povario <contact@povario.com>
2025-05-12 23:10:47 -0400

    Add copying code to clipboard

--- app/(app)/account.tsx
+++ app/(app)/account.tsx
@@ -3,11 +3,15 @@ import { useOtp } from "@/contexts/OtpProvider";
 import { StyleProp } from "@/util/StyleProp";
 import TouchVib from "@/util/TouchVib";
 import { router, useNavigation } from "expo-router";
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 import { Appbar, ProgressBar, Surface, Text, useTheme } from "react-native-paper";
+import * as Clipboard from "expo-clipboard";
+import * as Haptics from "expo-haptics";
+import { useToast } from "@/contexts/ToastProvider";
 
 const Account = () => {
   const otp = useOtp();
+  const toast = useToast();
   const navigation = useNavigation();
   const theme = useTheme();
 
@@ -43,20 +47,56 @@ const Account = () => {
   const progress = +(
     (otp.remainingTime ?? 0) / (otp.maxTime ?? 30)
   );
-
   const formattedCode = otp.code?.substring(0, otp.code.length / 2) + " " + otp.code?.substring(otp.code.length / 2);
+
+  const copy = async () => {
+    if (!otp.code) {
+      return;
+    }
+
+    try {
+      await Clipboard.setStringAsync(otp.code);
+      Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+    } catch (err) {
+      if (err instanceof Error) {
+        toast.error(err.message);
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+      }
+    }
+
+    await Clipboard.setStringAsync(otp.code);
+    await Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+  }
 
   const otpView = (
     <Surface
       style={styles.view}
       elevation={5}
     >
-      <Text style={styles.title} variant="headlineLarge">{otp.serviceName}</Text>
-      <Text style={{ marginBottom: 30 }} variant="headlineSmall">{otp.accountName}</Text>
+      <Text
+        style={styles.title}
+        variant="headlineLarge"
+      >
+        {otp.serviceName}
+      </Text>
+      
+      <Text
+        style={{ marginBottom: 30 }}
+        variant="headlineSmall"
+      >
+        {otp.accountName}
+      </Text>
 
-      <Text style={styles.code} variant="titleLarge">{formattedCode}</Text>
+      <Text
+        style={styles.code}
+        variant="titleLarge"
+        onPress={copy}
+      >
+        {formattedCode}
+      </Text>
+      
       <ProgressBar
-      style={styles.progressBar}
+        style={styles.progressBar}
         progress={progress}
       />
     </Surface>
